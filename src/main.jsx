d4046af4b6397d21454e1253d9317349c7ca571c
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-08-28 22:52:57 -0500

    Cambio

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     title="GENSHIN IMPACT" 
     title2="Travelers: Hydro, Anemo, Dendro, Electro"
     title3=" Personajes: Amber, Eula, Diluc, Sucrose, Raiden, Navia"
-    value={1}/>
+    value={10}/>
     <ComponentApp
     />
   </React.StrictMode>
