08e98870c31fcc9a4131f39cba37d4d225176f35
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-08-29 17:42:56 -0500

    Se hizo el challenge ultimo

--- src/ComponentApp.jsx
+++ src/ComponentApp.jsx
@@ -1,9 +1,33 @@
-const categories = ['first category', 'second category']
+import { useState } from "react"
 
 const ComponentApp =() => {
+
+    // Almacenamiento de las categorias
+    const[ categories, setCategories ] = useState([])
+    // Control del valor del Input Inicial
+    const[category, setCategory] = useState('')
+
+    // Funcion para agregar una categoria
+
+    const AddCategory = () => {
+          // Actualiza el estado de "categories" utilizando una función de actualización.
+        // La función recibe el estado anterior como argumento y devuelve el nuevo estado.
+        // Aquí, estamos agregando la nueva categoría al estado anterior usando el operador spread (...) y el valor de "category".
+        setCategories( list => [...list, category])
+        setCategory('')
+    }
+
+    const SetCategoryOtro = (evt) => {
+        setCategory( evt.target.value )
+    }
+
+
     return (
         <>
         <h1>GifExpert</h1>
+        <input type="text" value={category}
+        onChange={(event) => SetCategoryOtro(event)}/>
+        <button onClick={() => AddCategory()}> Añadir Categoria </button>
         <ol>
             {
                 categories.map(
@@ -11,8 +35,8 @@ const ComponentApp =() => {
                     {
                         return <li key={ key }>{ category }</li>
                     }
-                )
-            }
+                    )
+                }
         </ol>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <FirstApp 
     title="GENSHIN IMPACT" 
     title2="Travelers: Hydro, Anemo, Dendro, Electro"
-    title3=" Personajes: Amber, Eula, Diluc, Sucrose, Raiden, Navia"
+    title3=" Personajes: Amber, Eula, Diluc, Sucrose, Raiden, Kaeya"
     value={10}/>
     <ComponentApp
     />
