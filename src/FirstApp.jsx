ab317062e685de7c584850c17e8cc6194d54fb45
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-09-26 20:27:04 -0500

    Challenge 6

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,53 +1,37 @@
 import PropTypes from 'prop-types';
-import { useState } from "react";
+import { useState } from 'react';
+import useCounter from './Hooks/UseCounter';
 
-const FirstApp = ( {title, title2, title3, sum, value} ) => {
+const FirstApp = ({ title, title2, title3, sum, value }) => {
+  const { counter, increment, decrement, reset } = useCounter(value);
 
-  const [counter, setCounter] = useState( value )
-
-  const handleAdd = () => {
-    setCounter(counter + 1 )
-    console.log(counter);
-  }
-
-  const handleSubstract = () => {
-    setCounter(counter - 1)
-    console.log(counter)
-  }
-
-  const handleReset = () => {
-    setCounter(value)
-    console.log(counter)
-  }
-
-    return (
-      <>
-      <div> 
-        <h1>{ title }</h1>
-        <h2>{ title2 }</h2>
-        <h3>{ title3 }</h3>
-        <span>{ sum }</span>
-        <h1> Counter </h1>
-        <span> { counter } </span>
-        <button onClick={() => handleAdd()}> +1 </button>
-        <button onClick={() => handleReset()}> Reset </button>
-        <button onClick={() => handleSubstract()}> -1 </button>
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <h2>{title2}</h2>
+        <h3>{title3}</h3>
+        <span>{sum}</span>
+        <h1>Counter</h1>
+        <span> {counter} </span>
+        <button onClick={increment}>+1</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>-1</button>
       </div>
-      </>
-      )
-  } 
+    </>
+  );
+};
 
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  title2: PropTypes.string.isRequired,
+  title3: PropTypes.string.isRequired,
+  sum: PropTypes.number.isRequired,
+};
 
-  FirstApp.PropTypes = {
-    title: PropTypes.string.isRequired,
-    title2: PropTypes.string.isRequired,
-    title3: PropTypes.string.isRequired,
-    sum: PropTypes.number.isRequired
-  }
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  sum: 15,
+};
 
-  FirstApp.defaultProps = {
-    title: 'No hay titulo',
-    sum: 15
-  }
-  
-  export default FirstApp;
+export default FirstApp;
