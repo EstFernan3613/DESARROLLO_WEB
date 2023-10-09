05c125efc29dd4795d3cc81957587484faf6ce10
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-10-09 09:47:34 -0500

    Cambios Challenge 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ const App = () => {
   return (
     <>
     <div> 
-      <h1> Hola Mundo </h1>
-      <h2>Bienvenidos</h2>
+      <h1> Hola :D </h1>
+      <h2>Aqui se encuentra el primer Challenge</h2>
+      <h3> Challenge 1 - Acerca de las Funciones Arrow y Ejemplo</h3>
+      <h3> Se encuentra en la Carpeta Source llamado como: Challenge_01.js</h3>
     </div>
     </>
     )
