8b8f0c40a0313b2d9a891d121ba7de675bce6d7e
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-10-09 12:29:05 -0500

    Cambios Challenge 7

--- src/ComponentApp.jsx
+++ src/ComponentApp.jsx
@@ -1,31 +1,28 @@
-import React from "react"
-import { useState } from "react"
-import { AddCategory, GifGrid } from "./Components"
+import React from 'react';
+import { useFetch } from './Hooks/useFetch';
 
-const ComponentApp =() => {
+const ComponentApp = () => {
+  const { data, isLoading, hasError } = useFetch('https://api.giphy.com/v1/gifs/search?api_key=eAtY1pE7EXegmfV8ZO3dPR3c3ApG7Ndz&q=1&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips'); 
+  // En el apartado UseFetch agregas la URL la cual necesitas tener los datos
 
-    const[ categories, setCategories ] = useState([])
-
-    const OnAddCategory = ( category ) => {
-        setCategories( list => [...list, category])
-    }
-
-    return (
-        <>
-            <h1>Ingresa tu Personaje de Genshin</h1>
-
-            <AddCategory OnAddCategory={OnAddCategory}/>
-            {
-                categories.map(
-                    (category, key) =>
-                    {
-                        return <GifGrid category={category} key={key}/>
-                    }
-                )
-            }
-        </>
-    )    
+  return (
+    <div>
+      {isLoading ? (
+        <p>Cargando los datos solicitados</p>
+      ) : (
+        <div>
+          {hasError ? (
+            <p>Hubo un error al cargar los datos</p>
+          ) : (
+            <div>
+              <h2>Resultados de la busqueda</h2>
+              <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default ComponentApp;
-        
+export default ComponentApp
