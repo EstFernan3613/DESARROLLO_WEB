2715cafb7a136e5b8108143913a3d4282d27ba9f
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-10-09 10:03:01 -0500

    Cambios Challenge 3

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,37 +1,30 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import {useCounter} from "./Hooks/useCounter";
+import { useState } from 'react'
 
-const FirstApp = ({ title, title2, title3, sum, value }) => {
-  const { counter, increment, decrement, reset } = useCounter(value);
+const FirstApp = ({ value = 10 }) => {
+
+  const [counter, setCounter] = useState(value)
+
+  const handleAdd = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleSubtract = () => {
+    setCounter(counter - 1)
+  }
+
+  const handleReset = () => {
+    setCounter(value)
+  }
 
   return (
     <>
-      <div>
-        <h1>{title}</h1>
-        <h2>{title2}</h2>
-        <h3>{title3}</h3>
-        <span>{sum}</span>
-        <h1>Counter</h1>
-        <span> {counter} </span>
-        <button onClick={increment}>+1</button>
-        <button onClick={reset}>Resetear</button>
-        <button onClick={decrement}>-1</button>
-      </div>
+      <h1>Counter App</h1>
+      <h2>{ counter }</h2>
+      <button onClick={ handleAdd }>+1</button>
+      <button onClick={ handleSubtract }>-1</button>
+      <button onClick={ handleReset }>Reset</button>
     </>
-  );
-};
+  )
+}
 
-FirstApp.propTypes = {
-  title: PropTypes.string.isRequired,
-  title2: PropTypes.string.isRequired,
-  title3: PropTypes.string.isRequired,
-  sum: PropTypes.number.isRequired,
-};
-
-FirstApp.defaultProps = {
-  title: 'No hay titulo',
-  sum: 15,
-};
-
-export default FirstApp;
+export default FirstApp
