119e557100a0ede29c49a002faa85e7da6c19802
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-10-09 11:54:02 -0500

    Cambios Challenge 6

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import {useCounter} from "./Hooks/useCounter";
 
-const FirstApp = ({ title, title2, title3, sum, value }) => {
-  const { counter, increment, decrement, reset } = useCounter(value);
+const FirstApp = ({ title, title2, title3, sum, value = 1 }) => {
+  const { counter, increment, decrement, reset } = useCounter(value)
+  
 
   return (
     <>
@@ -12,8 +12,8 @@ const FirstApp = ({ title, title2, title3, sum, value }) => {
         <h2>{title2}</h2>
         <h3>{title3}</h3>
         <span>{sum}</span>
-        <h1>Counter</h1>
-        <span> {counter} </span>
+        <h1>Counter: Challenge 6</h1>
+        <h2> {counter} </h2>
         <button onClick={increment}>+1</button>
         <button onClick={reset}>Resetear</button>
         <button onClick={decrement}>-1</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import FirstApp from './FirstApp';
-import ComponentApp from './ComponentApp';
-import { Memorize } from './Components/Memorize';
-import { MainApp } from './pages/MainApp';
 import './index.css'
 import './App.css'
-import { TodoApp } from './TodoApp';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-     <FirstApp 
-     title="GENSHIN IMPACT" 
-     title2="Travelers: Hydro, Anemo, Dendro, Electro"
-     title3=" Personajes Iniciales: Amber, Lisa, Kaeya, Barbara, Xiangling"
-     value={10}/>
-     <ComponentApp
-     />
-     <Memorize />
-     <TodoApp />
+      <FirstApp title="Genshin Impact"
+       title2="Personajes actualmente disponibles en el juego" 
+       title3="Sujeto a cambios" 
+       sum={75} />
   </React.StrictMode>
-  // <BrowserRouter>
-  //   <MainApp />
-  // </BrowserRouter>
 )
