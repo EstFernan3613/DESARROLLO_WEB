1e6c4e7bba1a57d7d23b4814967fcec3dbbbf811
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-10-10 13:26:54 -0500

    Cambios Challenge 12

--- src/SharedComponents/NavComponent.jsx
+++ src/SharedComponents/NavComponent.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+// NavComponent.jsx
+
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom'
 
 export const NavComponent = () => {
   return (
-    <>
-        <nav>
-            <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Login</li>
-            </ul>
-        </nav>
-    </>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink activeClassName="active" to="/" exact>
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink activeClassName="active" to="/about">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink activeClassName="active" to="/login">
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { MainApp } from './pages/MainApp';
+import  { MainApp }  from './pages/MainApp';
 import './index.css'
 import './App.css'
+import './styles.css'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
