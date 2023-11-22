f96daf213386aec87f294c547bb0dcc5ba32ce8d
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-11-22 18:10:20 -0500

    Cambios Challenge 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-const App = () => {
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, incrementByAmount } from "./store/counterSlice";
+
+
+export const App = () => {
+
+  const { counter } = useSelector( state => state.counter );
+  const dispatch = useDispatch();
+
   return (
     <>
-    <div> 
-      <h1> Hola Mundo </h1>
-      <h2>Bienvenidos</h2>
-    </div>
-    </>
-    )
-} 
+      <h1>App Counter</h1>
+      <hr />
+      <span> Counter is { counter } </span>
 
-export default App;
+      <button 
+        className="btn btn-primary"
+        onClick={ () => dispatch(increment() )}
+        >  +1 </button>
+      <button 
+        className="btn btn-primary"
+        onClick={ () => dispatch(decrement() )}
+        >  -1 </button>
+      <button 
+        className="btn btn-primary"
+        onClick={ () => dispatch(incrementByAmount(5) )}
+        >  +5 </button>                
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import FirstApp from './FirstApp';
-import ComponentApp from './ComponentApp';
-import { Memorize } from './Components/Memorize';
-import { MainApp } from './pages/MainApp';
+import  { App }  from './App';
 import './index.css'
 import './App.css'
-import { TodoApp } from './TodoApp';
-import { LinkApp } from './LinkApp';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { store } from './store/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-        <MainApp />
-  </BrowserRouter>
-//     <React.StrictMode>
-//     <FirstApp 
-//     title="GENSHIN IMPACT" 
-//     title2="Travelers: Hydro, Anemo, Dendro, Electro"
-//     title3=" Personajes Iniciales: Amber, Lisa, Kaeya, Barbara, Xiangling"
-//     value={10}/>
-//     <ComponentApp
-//     />
-//     <Memorize />
-//     <TodoApp />
-//  </React.StrictMode>
+   <Provider store={store}>
+        <App />
+  </Provider>
 )
